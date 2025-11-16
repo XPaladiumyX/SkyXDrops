6ed720980625ad0f2c4949d1a94f0e10b2a77905
XPaladiumyX <allegra.yann@gmail.com>
2025-11-16 22:22:07 +0100

    first commit

---
+++ src/main/java/skyxnetwork/skyXDrops/SkyXDrops.java
@@ -0,0 +1,17 @@
+package skyxnetwork.skyXDrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyXDrops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
